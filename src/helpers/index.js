9146bb386b9cc04d33c138ff881e921348266cfe
Onohije <onohigbadume@gmail.com>
2025-01-03 19:01:53 +0100

    job update

---
+++ src/helpers/index.js
@@ -0,0 +1,12 @@
+const StateHook =()=>{
+    let  value ;
+    const setValue =(param)=>{
+      value = param
+      
+    }
+    
+    
+      return {value,setValue}
+  }
+  
+  export default StateHook;
